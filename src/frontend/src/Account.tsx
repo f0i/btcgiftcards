365e5bbf5f592ba351839c3be896d349143da43f
Martin Sigloch <f0i.git@projects.f0i.de>
2025-02-16 07:50:35 +0000

    Remove Bitcoin QR code

--- src/frontend/src/Account.tsx
+++ src/frontend/src/Account.tsx
@@ -8,7 +8,6 @@ import {
 import { GiftInfo } from "../../declarations/backend/backend.did";
 import { ckbtc_ledger } from "../../declarations/ckbtc_ledger";
 import { CopyButton } from "./CopyButton";
-import { QRCodeSVG } from "qrcode.react";
 import { encodeAccount, shortenErr, stringify } from "./utils";
 import { queries } from "./queryKeys";
 import toast from "react-hot-toast";
@@ -112,64 +111,6 @@ function UserInfo({
         : isError
           ? "Error: " + error
           : data?.toString() + " ckSat"}
-      <h3 className="mt-8">Connect your own wallet</h3>
-      If you already have ckBTC in your own wallet, you can connect it here:
-      <div className="flex flex-row gap-4 py-4">
-        <button className="button w-32">
-          <img className="h-16" src="/external/plug.svg" />
-          Plug wallet
-        </button>
-        <button className="button w-32">
-          <img className="h-16" src="/external/stoic.svg" />
-          Stoic wallet
-        </button>
-      </div>
     </div>
   );
 }
-
-function DepositAddressBTC(props: { info: GiftInfo; minter: MinterActor }) {
-  const account = props.info.account;
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ["deposit-address-btc", props.info.account.subaccount.toString()],
-    queryFn: () => {
-      return props.minter.get_btc_address({
-        owner: [account.owner],
-        subaccount: account.subaccount,
-      });
-    },
-  });
-
-  // TODO: remove dummy address
-  //return <BTCQRCode btcAddress="bc1qyawapemf4nsv6lc4z9tcltgfymsl2wklnecqlw" />;
-  if (isLoading) return <div>Loading BTC deposit address...</div>;
-  // TODO: log error
-  if (isError) return <div>Error getting BTC deposit address.</div>;
-  if (!data) return <div>No Data received</div>;
-  return <BTCQRCode btcAddress={data} />;
-}
-
-const BTCQRCode = ({ btcAddress }: { btcAddress: string }) => {
-  const btcUri = `bitcoin:${btcAddress}`;
-
-  return (
-    <div className="">
-      BTC deposit account:{" "}
-      <CopyButton label="Copy BTC Deposit Address" textToCopy={btcAddress} />
-      <br />
-      <div className="info-address min-height-200 flex-row">
-        <span className="max-w-600 flex-grow">{btcAddress}</span>
-        <QRCodeSVG
-          width={150}
-          className="float-right w-300"
-          value={btcUri}
-          size={150} // in pixels
-          fgColor="#000000"
-          bgColor="transparent"
-          level="H" // Error correction: L, M, Q, H
-          marginSize={1}
-        />
-      </div>
-    </div>
-  );
-};
